d0de20e52206cecf929839290e837a966c600d47
Clemens Radermacher <clemera@posteo.net>
2019-04-23 19:43:00 +0200

    Update docstring

--- frog-menu.el
+++ frog-menu.el
@@ -593,7 +593,7 @@ ACTIONS is the argument of `frog-menu-read'."
 (defun frog-menu-call (&rest cmds)
   "Read a command from CMDS and execute it.
 
-CMDS is a list of command symbols to choose from."
+CMDS are command symbols to choose from."
   (let ((cmd (intern-soft (frog-menu-read "" (mapcar #'symbol-name cmds)))))
 	(unless (commandp cmd)
 	  (user-error "%s is no a kown command" cmd))
