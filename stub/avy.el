897955cac996c008679d72639a7b1327349165ca
Stefan Monnier <monnier@iro.umontreal.ca>
2019-08-19 06:15:21 -0400

    * stub/posframe.el, stub/avy.el: Add missing copyright blurb.

--- stub/avy.el
+++ stub/avy.el
@@ -1,3 +1,20 @@
+;;; stub/avy.el --- ??? -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Free Software Foundation, Inc.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 (defvar avy-keys nil)
 (defvar avy-single-candidate-jump nil)
 (defvar avy-handler-function nil)
@@ -9,4 +26,5 @@
 (defun avy--process (candidates overlay-fn))
 (defun avy--style-fn (style))
 
-(provide 'avy)
+(provide 'avy) ;; FIXME: Really?
+;;; stub/posframe.el ends here
