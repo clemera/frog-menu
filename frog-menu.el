3d99c10eb472b5f7fe761bb3f329fe10971ea903
Clemens Radermacher <clemera@posteo.net>
2020-05-21 17:00:46 +0200

    Bump version

--- frog-menu.el
+++ frog-menu.el
@@ -4,7 +4,7 @@
 
 ;; Author: Clemens Radermacher <clemera@posteo.net>
 ;; URL: https://github.com/clemera/frog-menu
-;; Version: 0.2.10
+;; Version: 0.2.11
 ;; Package-Requires: ((emacs "26") (avy "0.4") (posframe "0.4"))
 ;; Keywords: convenience
 
